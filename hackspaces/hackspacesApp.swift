5caa36c30381f783ab525d5dd5d6d069f34e719f
Florian Bruder <florian.bruder@posteo.de>
2022-05-04 22:59:53 +0200

    Initial Xcode project commit

---
+++ hackspaces/hackspacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackspacesApp.swift
+//  hackspaces
+//
+//  Created by Florian Bruder on 04.05.22.
+//
+
+import SwiftUI
+
+@main
+struct hackspacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hackspacesUITests/hackspacesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  hackspacesUITestsLaunchTests.swift
+//  hackspacesUITests
+//
+//  Created by Florian Bruder on 04.05.22.
+//
+
+import XCTest
+
+class hackspacesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
